da04903f47353bed67a62ec43c2fb5538d07ba34
Ben Wicke <ben@ciagent.com>
2016-03-24 15:35:40 -0400

    scre

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,7 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Costrecovery::Application.config.secret_token = '8982ebd0ca88b7a06daa4545dce71304b6bc95aa7128583528bb303e80c9a9708b5a62864f83b3f518f34088e4ffc79853505adad268e8078493b6acf8bd7a9f'
+Costrecovery::Application.config.secret_token = SecureRandom.hex(64)
+
+
+#'8982ebd0ca88b7a06daa4545dce71304b6bc95aa7128583528bb303e80c9a9708b5a62864f83b3f518f34088e4ffc79853505adad268e8078493b6acf8bd7a9f'
 
 
 
