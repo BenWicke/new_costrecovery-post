59ebe09c6cd20d353c90e11e0c1071f0bdaf438a
Ben Wicke <ben@ciagent.com>
2016-03-24 15:59:11 -0400

    scre2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,12 +5,3 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Costrecovery::Application.config.secret_token = SecureRandom.hex(64)
-
-
-#'8982ebd0ca88b7a06daa4545dce71304b6bc95aa7128583528bb303e80c9a9708b5a62864f83b3f518f34088e4ffc79853505adad268e8078493b6acf8bd7a9f'
-
-
-
-#ENV['APP_SECRET']
-
-
