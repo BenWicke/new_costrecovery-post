5dec5de62d1923988eeddef490a20b7358adf4e9
Zeinab <zeinab@ciagent.com>
2016-09-15 15:08:02 -0400

    Revert "Edits to secret token"
    This reverts commit 8abd44e797b4048faf9a128f6e117f8923a8cd69.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Costrecovery::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
-  ('x' * 30) # meets minimum requirement of 30 chars long
-else
-  ENV['APP_SECRET']
-end
+Costrecovery::Application.config.secret_token = SecureRandom.hex(64)
